b29af6284ead293d3154759df769ea4adf2f70fb
Ghalia Sbei <ghaliaprincesssbei@gmail.com>
2026-01-03 09:38:24 +0300

    Finished

--- src/Main.java
+++ src/Main.java
@@ -17,43 +17,42 @@ import SymbolTable.TemplateSymbolTable;
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        Path p = Paths.get("src/Tests/FinalTests/app.py").toAbsolutePath();
+        Path p = Paths.get("src/Tests/FinalTests/base.html").toAbsolutePath();
         CharStream input = CharStreams.fromPath(p);
-                    /////////////////////////////
+
                         //Flask Compiler//
 
-        FlaskLexer lexer = new FlaskLexer(input);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        FlaskParser parser = new FlaskParser(tokens);
+//        FlaskLexer lexer = new FlaskLexer(input);
+//        CommonTokenStream tokens = new CommonTokenStream(lexer);
+//        FlaskParser parser = new FlaskParser(tokens);
+//
+//        ProgramVisitor visitor = new ProgramVisitor();
+//        Program program = visitor.visit(parser.prog());
+//        ASTGraphvizPrinter.print(program, "ast.dot");
+//
+//        FlaskSymbolTable flaskSymbolTable = new FlaskSymbolTable();
+//        SymbolTable.SymbolTableVisitor stVisitor = new SymbolTable.SymbolTableVisitor();
+//        stVisitor.visitProgram(program);
+//        flaskSymbolTable = stVisitor.getSymbolTable();
+//        flaskSymbolTable.printSymbolTable();
 
-        ProgramVisitor visitor = new ProgramVisitor();
-        Program program = visitor.visit(parser.prog());
-        ASTGraphvizPrinter.print(program, "ast.dot");
-
-        FlaskSymbolTable flaskSymbolTable = new FlaskSymbolTable();
-        SymbolTable.SymbolTableVisitor stVisitor = new SymbolTable.SymbolTableVisitor();
-        stVisitor.visitProgram(program);
-        flaskSymbolTable = stVisitor.getSymbolTable();
-        flaskSymbolTable.printSymbolTable();
-
-                    /////////////////////////////
 
                          //Template Compiler//
 
-//        TemplateLexer lexer = new TemplateLexer(input);
-//        CommonTokenStream tokens = new CommonTokenStream(lexer);
-//        TemplateParser parser = new TemplateParser(tokens);
-//
-//        TemplateVisitor visitor = new TemplateVisitor();
-//        ASTNode root = visitor.visit(parser.template());
-//        ASTGraphvizPrinter.print(root, "ast.dot");
-//
-//
-//        TemplateSymbolTable templateSymbolTable = new TemplateSymbolTable();
-//        JinjaASTVisitor stVisitor = new JinjaASTVisitor(templateSymbolTable);
-//        stVisitor.visit(root);
-//        templateSymbolTable.print();
-//
+        TemplateLexer lexer = new TemplateLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        TemplateParser parser = new TemplateParser(tokens);
+
+        TemplateVisitor visitor = new TemplateVisitor();
+        ASTNode root = visitor.visit(parser.template());
+        ASTGraphvizPrinter.print(root, "ast.dot");
+
+
+        TemplateSymbolTable templateSymbolTable = new TemplateSymbolTable();
+        JinjaASTVisitor stVisitor = new JinjaASTVisitor(templateSymbolTable);
+        stVisitor.visit(root);
+        templateSymbolTable.print();
+
 
     }
 }
